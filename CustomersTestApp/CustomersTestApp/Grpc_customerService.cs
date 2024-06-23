ccf91ef427cd923ff1514824b4fa678dd9e909f2
AayushiChoksi <choksi.aa@northeastern.edu>
2024-06-23 10:19:48 -0400

    Refactor and implement gRPC Customer Service
    - Refactored the constructor to initialize customer list with predefined customers.
    - Implemented GetCustomersArray() method to return array of customers.
    - Added methods for adding, updating, and removing customers:
    - AddCustomer() to add a new customer to the list.
    - UpdateCustomer() to update existing customer details.
    - RemoveCustomer() to remove a customer by ID if removable.
    - Implemented GetCustomers() method to asynchronously return customers.

--- CustomersTestApp/CustomersTestApp/Grpc_customerService.cs
+++ CustomersTestApp/CustomersTestApp/Grpc_customerService.cs
@@ -1,59 +1,90 @@
-﻿namespace CustomersTestApp;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CustomersTestApp.Models;
 
-public class Grpc_customerService : ICustomerService
+namespace CustomersTestApp
 {
-    public Customer[] get_cust()
+    public class Grpc_customerService : ICustomerService
     {
-        return _List1;
-    }
+        private List<Customer> _customers;
 
-    #region Constructors
+        #region Constructors
 
-    public Grpc_customerService()
-    {
-    }
-
-    #endregion Constructors
-
-    private Customer[] _List1 =
-    [
-        new Customer
+        public Grpc_customerService()
         {
-            Id = Guid.NewGuid(),
-            name = "Microsoft",
-            Email = "microsoft@microsoft.com",
-            Discount = 10,
-            Can_Remove = false
-        },
-        new Customer
-        {
-            Id = Guid.NewGuid(),
-            name = "Google",
-            Email = "google@google.com",
-            Discount = 5,
-            Can_Remove = false
-        },
-        new Customer
-        {
-            Id = Guid.NewGuid(),
-            name = "Amazon",
-            Email = "amazon@amazon.com",
-            Discount = 0,
-            Can_Remove = true
+            _customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Name = "Microsoft",
+                    Email = "microsoft@microsoft.com",
+                    Discount = 10,
+                    Can_Remove = false
+                },
+                new Customer
+                {
+                    Name = "Google",
+                    Email = "google@google.com",
+                    Discount = 5,
+                    Can_Remove = false
+                },
+                new Customer
+                {
+                    Name = "Amazon",
+                    Email = "amazon@amazon.com",
+                    Discount = 0,
+                    Can_Remove = true
+                }
+            };
         }
-    ];
 
-    public Task<bool> AddCustomer(Customer c)
-    {
-        throw new NotImplementedException();
+        #endregion Constructors
+
+        public Customer[] GetCustomersArray()
+        {
+            return _customers.ToArray();
+        }
+
+        public Task<bool> AddCustomer(Customer c)
+        {
+            _customers.Add(c);
+            return Task.FromResult(true);
+        }
+
+        public IAsyncEnumerable<Customer> GetCustomers(CancellationToken cancellationToken)
+        {
+            return _customers.ToAsyncEnumerable();
+        }
+
+        public Task<bool> UpdateCustomer(Customer c)
+        {
+            var existingCustomer = _customers.FirstOrDefault(cust => cust.Id == c.Id);
+            if (existingCustomer != null)
+            {
+                existingCustomer.Name = c.Name;
+                existingCustomer.Email = c.Email;
+                existingCustomer.Discount = c.Discount;
+                existingCustomer.Can_Remove = c.Can_Remove;
+                return Task.FromResult(true);
+            }
+            return Task.FromResult(false);
+        }
+
+        public Task<bool> RemoveCustomer(string id)
+        {
+            var customer = _customers.FirstOrDefault(c => c.Id == id);
+            if (customer != null && customer.Can_Remove)
+            {
+                _customers.Remove(customer);
+                return Task.FromResult(true);
+            }
+            return Task.FromResult(false);
+        }
+
+        // No database is needed
+        // Assume we have a perfect service that is always online.
     }
-
-    public IAsyncEnumerable<Customer> GetCustomers(CancellationToken cancellationToken)
-    {
-        return _List1.ToAsyncEnumerable();
-    }
-
-    // todo implement an asynchronous CRUD
-    // No database is needed
-    // Assume we have a perfect service that is always online.
 }
